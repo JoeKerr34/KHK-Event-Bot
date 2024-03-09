bf2bf2ba301dab29b841f9de392cc69ad92b1b73
Kshitij Shrinivas Dhande <142138023+kshitijd-2004@users.noreply.github.com>
2024-03-08 21:08:07 -0600

    Add files via upload
    Added command "events". Fixed token issue

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
+import json
 
 client = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+config_file = open("config.json")
+config = json.load(config_file)
 
 
 @client.event
@@ -14,9 +18,8 @@ async def on_ready():
 # Shows the events in the current week
 @client.command()
 async def events(ctx):
-
     # Display the events for the week
     await ctx.send("Here are the events for the week")
 
 
-client.run('MTIxNTg0MjI1ODkzOTU0MzU2Mw.GNeAXs.m0nn8Y2uEe4G1-VPVYe0DsBbn-FLxv4JYQTtdE')
+client.run(config["bot_token"])
